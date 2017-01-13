7301e76c21a9918b971f07663a9dc4cc5c7d36ac
Sagarwal-incise <agarwalshubham025@gmail.com>
2017-01-13 15:09:08 +0530

    Add files via upload

---
+++ spi_bkbiet/spi_bkbiet/spi_lib.c
@@ -0,0 +1,17 @@
+/*
+ * spi_bkbiet.c
+ *
+ * Created: 12-Jan-17 3:43:29 PM
+ *  Author: Agarwal-pc
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ spi_bkbiet/spi_master/spi_master.c
@@ -0,0 +1,17 @@
+/*
+ * spi_master.c
+ *
+ * Created: 12-Jan-17 3:45:29 PM
+ *  Author: Agarwal-pc
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
